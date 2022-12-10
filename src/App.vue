fba3671a824867b8990e82614d61ed52ea159e12
markus <datamastery87@gmail.com>
2022-12-10 08:38:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <h2>App</h2>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
